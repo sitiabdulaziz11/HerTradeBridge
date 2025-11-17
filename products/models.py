d5439564567f6763950003494e1ae4cc1ce94fe6
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2025-11-17 17:01:08 +0300

    e-co

--- products/models.py
+++ products/models.py
@@ -13,16 +13,19 @@ class Product(models.Model):
         ]
     
     name = models.CharField(max_length=200)
-    description = models.TextField() 
     # price = models.DecimalField(max_digits=10, decimal_places=2)
-    price = models.CharField(max_length=20)
-    image = models.ImageField(upload_to='products/images') 
-    video = models.FileField(upload_to='products/videos/', blank=True, null=True)
+    price = models.CharField(max_length=20,  blank=True, null=True)
+    description = models.TextField()
+    
     weight = models.CharField(max_length=50, blank=True, null=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='available')
     
     seller = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='products')
     address = models.ForeignKey(Address, on_delete=models.SET_NULL, null=True, blank=True)
+    
+    image = models.ImageField(upload_to='products/images') 
+    video = models.FileField(upload_to='products/videos/', blank=True, null=True)
+    
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
