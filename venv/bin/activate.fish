502f96894aa83c7ef58bacabde6a1fd5d7b375b1
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2025-11-25 15:46:11 +0300

    recreated env/db

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/alhemdulillah/HerTradeBridge/venv
+set -gx VIRTUAL_ENV /home/alhemdulillah/projects/HerTradeBridge/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
